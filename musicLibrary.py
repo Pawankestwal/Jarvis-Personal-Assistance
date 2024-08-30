7f626da6e6656547465824dbc6cf91ebf10e92ee
PAWAN A KESTWAL <101412114+Pawankestwal@users.noreply.github.com>
2024-08-30 13:29:35 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "love": "https://youtu.be/EHvbWD6KOPc?si=y4LkF_kuZTOxxwhg",
+    "party": "https://youtu.be/J4meiPiFSuA?si=0-HirNmNzK8pzMpN",
+    "old": "https://youtu.be/nltNdj7bJo8?si=zw3W3gCgrt2FM9Ms",
+    "hindi": "https://youtu.be/zoFLbJ_09aM?feature=shared"
+}
